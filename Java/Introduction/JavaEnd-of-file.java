192eaf431a73ff768319cad4f1c530c3db92f4ec
Mamunur Rashid Rimon <rimonmath@gmail.com>
2017-10-04 12:04:38 +0600

    Create JavaEnd-of-file.java

---
+++ Java/Introduction/JavaEnd-of-file.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int i=0;
+        
+        while(s.hasNext()){
+            i++;
+            System.out.println(i + " " +s.nextLine());
+        }
+    }
+}
