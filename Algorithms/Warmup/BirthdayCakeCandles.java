4dc409a3b778bd521fc26d313db5803a4099f7e2
Mamunur Rashid Rimon <rimonmath@gmail.com>
2018-05-10 14:10:24 +0600

    Create BirthdayCakeCandles.java

---
+++ Algorithms/Warmup/BirthdayCakeCandles.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the birthdayCakeCandles function below.
+     */
+    static int birthdayCakeCandles(int n, int[] ar) {
+        /*
+         * Write your code here.
+         */
+        int big = ar[0];
+        int count = 1;
+        
+        
+        for(int i=1; i< ar.length; i++){
+            if(ar[i] > big ){
+                big = ar[i];
+                count = 1;
+            } else if(big == ar[i]){
+                count += 1;
+            }
+        }
+        
+        return count;
+
+    }
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(scan.nextLine().trim());
+
+        int[] ar = new int[n];
+
+        String[] arItems = scan.nextLine().split(" ");
+
+        for (int arItr = 0; arItr < n; arItr++) {
+            int arItem = Integer.parseInt(arItems[arItr].trim());
+            ar[arItr] = arItem;
+        }
+
+        int result = birthdayCakeCandles(n, ar);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+    }
+}
